a5abdca8aadcc9d3316c7e464566af7ad0350c45
Pamento <pamento.dev@gmail.com>
2021-02-16 14:11:53 +0100

    In express set template jade for message; remove network.js file in client/

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -13,12 +14,14 @@ const clientApp = require('./routes/spa');
 
 const app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // prevent default favicon of Express
 app.use(ignoreFavicon);
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,27 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { sendEmail } = require('../controllers/contact');
 
 /* POST contact/message listing. */
-router.post('/message', function (req, res, next) {
-  // req.body:
-  // {
-  //   status: '',
-  //   title: '',
-  //   firstName: 'Pino',
-  //   lastName: 'Opinel',
-  //   email: 'name@eman.com',
-  //   subject: '',
-  //   message: "Bonjour, ...!"
-  // }
-  // next: check if each value is a string and send the email.
-  
-  console.dir(req.body);
-
-
-  // res.status(200);
-  res.setHeader('Content-Type', 'application/json');
-  // res.end(JSON.stringify({"success": "OK"}));
-  res.json({success: OK});
-});
+router.post('/message', sendEmail);
 
 module.exports = router;
