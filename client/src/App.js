4858ae160bec1161cfc102b10f8c74409d6d87f3
Pamento <pamento.dev@gmail.com>
2021-03-12 12:13:37 +0100

    Add cookies consent

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './AppM.css';
 import './AppS.css';
 import './i18n';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import Header from "./components/containers/HomeHeader";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -11,9 +12,11 @@ import TestimonyAndGallery from "./pages/TestimonyAndGallery";
 import Faq from "./pages/Faq";
 import Contact from "./pages/Contact";
 import Footer from "./components/containers/Footer";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
 
+  const { t } = useTranslation();
   const options = { year: 'numeric', month: 'long' }
   const today = new Date();
 
@@ -21,7 +24,6 @@ function App() {
     return today.toLocaleDateString("en-US", options);
   }
   const newDate = getDateAtMoment();
-  // setDate(newDate);
 
   return (
     <div className="App">
@@ -37,6 +39,10 @@ function App() {
         </Switch>
         <Footer />
         <p className="home-author"><a href="https://fr.linkedin.com/in/pawel-grzesik" target="_blank" rel="noopener noreferrer">Pamento | </a> <span>{newDate}</span> </p>
+        <CookieConsent
+        buttonText="OK"
+        style={{ background: "#FC5C4D" }}
+        buttonStyle={{ background: "#001252", color: "#FFFFFF", fontSize: "13px" }} >{t('global.cookies-consent')}</CookieConsent>
       </Router>
     </div>
   );
